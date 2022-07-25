95fcb26d5c7c8d9cf6d751026a533ef5946969b8
hernandezzavala <piratecorexd@gmail.com>
2022-07-25 12:07:59 -0500

    API Funcionando

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator")
 
 const checkErrors = (request, response, next) => {
     const errors = validationResult(request);
-    if(!errors.isEmpty()) {
+    if(errors.length > 1) {
         return response.status(400).json(errors);
     }
     next();
